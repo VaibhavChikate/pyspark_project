41570a8e2b85491675980ed4d7d74f43e456ea28
VaibhavChikate <87650557+VaibhavChikate@users.noreply.github.com>
2021-07-27 08:38:01 +0530

    Update save table in mysql.py

--- pyspark_project/save table in mysql.py
+++ pyspark_project/save table in mysql.py
@@ -4,7 +4,7 @@ spark=SparkSession.builder.appName("testing").master("local").getOrCreate()
 df=spark.read.json("C:\\Bigdata\\Datasets\\cars.json")
 #print(df.count())
 #df.show()
-url="jdbc:mysql://mysqldb.c5logfp8eply.ap-south-1.rds.amazonaws.com:3306/sqldb"
+url="jdbc:mysql://mysqldb.31skjnfss.ap-south-1.rds.amazonaws.com:3306/sqldb"
 df.write.format("jdbc").option("url",url).option("user","username").option("password","password").option("driver","com.mysql.cj.jdbc.Driver")\
     .option("dbtable","cars").mode("overwrite").save()
 print("succefully table is exported")
